bcc5e3b93777271148090f05cf921723f4dfd9ee
Fernando Johnson <fernandojohnson28@yahoo.com>
2020-08-11 11:15:01 +0000

    Fernando Johnson
    This is my React Assignment

---
+++ precache-manifest.083bbf2ff59218160b631ea85f34729d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6aeb5fdc8b3c85f22a6c9ba428256e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c500ce2f5ab62a43bed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7229a800b0a402ba417a",
+    "url": "/static/js/2.ecbf96d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ecbf96d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c500ce2f5ab62a43bed",
+    "url": "/static/js/main.49be7943.chunk.js"
+  },
+  {
+    "revision": "539ba9d48885745e6441",
+    "url": "/static/js/runtime-main.25a16609.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
